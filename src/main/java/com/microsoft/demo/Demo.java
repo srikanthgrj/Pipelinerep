4b3df8ecad4e052a5c1d4e2e1f397ca3314dfc4b
srikanthgrj <60284704+srikanthgrj@users.noreply.github.com>
2022-01-10 12:37:33 +0530

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -9,6 +9,6 @@ public class Demo {
 
         System.out.println("I am not covered");
         System.out.println("I am not covered");
-        System.out.println("I am not covered");
+        
     }
 }
